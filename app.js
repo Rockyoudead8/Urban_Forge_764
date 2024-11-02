a41ef9983430488291137be9b19ff9642673f05b
Rockyoudead8 <arpitgoyal1941977@gmail.com>
2024-11-02 15:05:09 +0530

    updated

--- app.js
+++ app.js
@@ -2,23 +2,72 @@ const express = require("express")
 const app = express()
 const usermodel = require("./models/user")
 const path = require("path")
-const sellermodel=require("./models/seller")
+const sellermodel = require("./models/seller")
+const productmodel = require("./models/inventory")
 const { create } = require("domain")
 const user = require("./models/user")
+const multer = require("multer")
 const bcrypt = require('bcrypt');
+const crypto = require("crypto")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "/public")))
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+        crypto.randomBytes(12, function (err, bytes) {
+            const fn = bytes.toString("hex") + path.extname(file.originalname)
+            cb(null, fn)
+        })
+
+    }
+})
+
+const upload = multer({ storage: storage })
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
-app.get("/seller-signup",(req,res)=>{
+app.get("/seller-signup", (req, res) => {
     res.render("seller.ejs")
 })
 
+app.get("/products", async (req, res) => {
+    let allproducts = await productmodel.find()
+    res.render("products.ejs", { allproducts })
+})
 
+app.post("/createproduct", upload.single("Image"), async (req, res) => {
+    const { productid } = req.body; 
+
+    // Find a user with the given email
+    const id = await productmodel.findOne({ productid });
+
+    if (!id) {
+        let createdproduct = await productmodel.create({
+            // productpic: `/images/uploads/${req.file.filename}`,
+            productname: req.body.productname,
+            productid: req.body.productid,
+            price: req.body.Price,
+            quantity: req.body.quantity,
+            category: req.body.category,
+            size: req.body.size,
+            description: req.body.desc,
+            story: req.body.story
+        })
+        res.redirect("/products")
+    }
+ 
+    else{
+     res.redirect("/admin");
+    }
+    
+})
 
 app.post("/create", async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10); // Hash the password
@@ -37,16 +86,26 @@ app.post("/createseller", async (req, res) => {
         email: req.body.email,
         password: hashedPassword
     });
-    res.redirect("/seller-signup")
+    res.redirect("/admin")
 });
 
-app.get("/login1",(req,res)=>{
+app.get("/admin", (req, res) => {
+    res.render("adminpanel.ejs")
+})
+
+app.get("/login1", (req, res) => {
     res.render("login1.ejs")
 })
 
-app.get("/login",(req,res)=>{
-    res.render("login.ejs")
+app.get("/login", (req, res) => {
+    res.redirect("login1")
 })
+
+app.get("/index", (req, res) => {
+    res.redirect("/seller-signup")
+})
+
+
 
 app.post("/read", async (req, res) => {
     const { email, password } = req.body; // Get email and password from the request body
@@ -62,7 +121,7 @@ app.post("/read", async (req, res) => {
 
     // Check if the provided password matches the stored password
     // Assuming you're using bcrypt for password hashing
-    const isMatch = await bcrypt.compare(password, user.password); 
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
         return res.render("login.ejs", {
@@ -88,7 +147,7 @@ app.post("/sread", async (req, res) => {
 
     // Check if the provided password matches the stored password
     // Assuming you're using bcrypt for password hashing
-    const isMatch = await bcrypt.compare(password, seller.password); 
+    const isMatch = await bcrypt.compare(password, seller.password);
 
     if (!isMatch) {
         return res.render("login1.ejs", {
@@ -97,7 +156,7 @@ app.post("/sread", async (req, res) => {
     }
 
     // If credentials are valid, redirect to the main page
-    return res.redirect("/seller-signup");
+    return res.redirect("/admin");
 });
 
 
---
+++ models/inventory.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/beyonce_botique")
+
+const inventoryschema=mongoose.Schema({
+    // productpic : String,
+    productname : String,
+    productid : String,
+    price : Number,
+    quantity: Number,
+    category : String,
+    size :String,
+    description :String,
+    story : String
+    })                                                                                                          
+
+module.exports=mongoose.model("inventory",inventoryschema)
