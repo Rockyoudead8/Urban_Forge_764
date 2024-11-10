9ba6f217513cad858ce1fe6e0487487bb7e70a84
Arpit Goyal <arpitgoyal1941977@gmail.com>
2024-11-10 10:35:28 +0530

    new files

--- models/inventory.js
+++ models/inventory.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose")
 
 mongoose.connect("mongodb://127.0.0.1:27017/beyonce_botique")
 
-const inventoryschema=mongoose.Schema({
-    // productpic : String,
-    productname : String,
-    productid : String,
-    price : Number,
+const inventoryschema = mongoose.Schema({
+    image: String,
+    productname: String,
+    productid: String,
+    price: Number,
     quantity: Number,
-    category : String,
-    size :String,
-    description :String,
-    story : String
-    })                                                                                                          
+    category: String,
+    size: String,
+    description: String,
+    story: String
+})
 
-module.exports=mongoose.model("inventory",inventoryschema)
+module.exports = mongoose.model("inventory", inventoryschema)
